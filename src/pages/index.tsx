7364e555aef0a950f972d666f300f240e36ba51c
TbotaPhantA <botakiller@mail.ru>
2023-04-06 20:46:10 +0000

    first title styles were added.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={homePageStyles.homePage}>Hello, world!</main>
+      <main className={homePageStyles.homePage}>
+        <div className={homePageStyles.title}>Hello, there 1</div>
+      </main>
     </>
   )
 }
