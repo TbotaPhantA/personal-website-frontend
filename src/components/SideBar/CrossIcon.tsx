8d652a7d6b4511c3b39450e61887c034b8816c56
TbotaPhantA <botakiller@mail.ru>
2023-04-08 20:41:57 +0000

    not existing class was removed.

--- src/components/SideBar/CrossIcon.tsx
+++ src/components/SideBar/CrossIcon.tsx
@@ -1,11 +1,10 @@
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import crossCustomIcon from '../../styles/components/SideBar.module.scss'
 
 export function CrossIcon() {
   return (
-    <div className={crossCustomIcon.BmCrossCustomIcon}>
+    <div>
       <FontAwesomeIcon icon={faTimes} size="3x" color="#DE004E" />
     </div>
   )
