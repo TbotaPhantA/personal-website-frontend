7caf3b4be9f0e72b5836da03d4beea73af98c66a
TbotaPhantA <botakiller@mail.ru>
2023-04-08 22:38:21 +0000

    minor hover animation fixes

--- src/components/SideBar/CrossIcon.tsx
+++ src/components/SideBar/CrossIcon.tsx
@@ -5,7 +5,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export function CrossIcon() {
   return (
     <div>
-      <FontAwesomeIcon icon={faTimes} size="3x" color="#DE004E" />
+      <FontAwesomeIcon icon={faTimes} size="3x" />
     </div>
   )
 }
