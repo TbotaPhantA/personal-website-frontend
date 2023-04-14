f5591b9427c327376aa07801c448b835be8f3ba5
TbotaPhantA <botakiller@mail.ru>
2023-04-14 20:07:45 +0000

    translations for about anchor was added.

--- src/anchors/about.tsx
+++ src/anchors/about.tsx
@@ -1,15 +1,21 @@
 import aboutStyles from '../styles/anchors/About.module.scss';
+import { useRouter } from 'next/router';
+import { chooseTranslation } from '@/shared/utils/chooseTranslation';
 
 interface HomeProps {
   id: string;
 }
 
 export default function About({ id }: HomeProps) {
+  const { locale } = useRouter();
+  const t = chooseTranslation(locale);
+  const { about } = t.pages.main;
+
   return (
     <div id={id} className={aboutStyles.AboutPage}>
       <div className={aboutStyles.AboutPageTitle} data-aos="fade-left">
-        <div>{"About Me"}</div>
-        <div className={aboutStyles.AboutPageContent}>{"\"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\""}</div>
+        <div>{about.title}</div>
+        <div className={aboutStyles.AboutPageContent}>{about.description}</div>
       </div>
     </div>
   )
