1f976738a5a63ec98bcce183acbd8b5aa54fac4e
TbotaPhantA <botakiller@mail.ru>
2023-04-20 21:13:31 +0000

    translation for book review was added.

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,18 +1,27 @@
 import { BookReview } from '@/shared/types/bookReview';
+import { useRouter } from 'next/router';
 
 interface BooksListProps {
   bookReviews: BookReview[],
 }
 
 export default function BooksList({ bookReviews }: BooksListProps) {
+  const { locale } = useRouter();
+
   return (
     <div>
-      {bookReviews.map(review => (
-        <div key={review.id}>
-          <div>{review.article.originalTitle}</div>
-          <div>{review.article.originalContent}</div>
-        </div>
-      ))}
+      {bookReviews.map(review => {
+        const translatedReview = review.article.translations.find(t => t.languageId === locale)
+        const title = translatedReview?.title ?? review.article.originalTitle;
+        const content = translatedReview?.content ?? review.article.originalContent;
+
+        return (
+          <div key={review.id}>
+            <div>{title}</div>
+            <div>{content}</div>
+          </div>
+        )
+      })}
     </div>
   )
 }
