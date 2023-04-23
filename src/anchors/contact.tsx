1134cbd42b4bff0cd60b285e99fda13a75d4045c
TbotaPhantA <botakiller@mail.ru>
2023-04-23 12:55:48 +0000

    fade-right was added for contact title

--- src/anchors/contact.tsx
+++ src/anchors/contact.tsx
@@ -3,7 +3,7 @@ import contactStyles from '../styles/anchors/Contact.module.scss';
 export function Contact() {
   return (
     <div id={'contact'} className={contactStyles.ContactAnchor}>
-      <div className={contactStyles.ContactAnchorTitle}>Contact Me</div>
+      <div className={contactStyles.ContactAnchorTitle} data-aos="fade-right">Contact Me</div>
     </div>
   )
 }
