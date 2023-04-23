d588622a3410e0a3e3280316ee68c76cdea3b1fd
TbotaPhantA <botakiller@mail.ru>
2023-04-23 12:52:26 +0000

    title for contac page was added.

--- src/anchors/contact.tsx
+++ src/anchors/contact.tsx
@@ -3,7 +3,7 @@ import contactStyles from '../styles/anchors/Contact.module.scss';
 export function Contact() {
   return (
     <div id={'contact'} className={contactStyles.ContactAnchor}>
-      Contact Info
+      <div className={contactStyles.ContactAnchorTitle}>Contact Me</div>
     </div>
   )
 }
