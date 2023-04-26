9b25e17e605ce3a7a48a1e7c3fe9fcb6ac04f101
TbotaPhantA <botakiller@mail.ru>
2023-04-26 19:30:56 +0000

    unstyled book review modal window was added.

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import { BookReview } from '@/shared/types/bookReview';
 import { useRouter } from 'next/router';
 import booksListStyles from '@/styles/components/BookList/BookList.module.scss';
+import Modal from 'react-modal';
 
 interface BooksListProps {
   bookReviews: BookReview[],
 }
 
 export default function BooksList({ bookReviews }: BooksListProps) {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [selectedBookReview, setSelectedBookReview] = useState<BookReview | undefined>(undefined)
   const { locale } = useRouter();
+
+  function openModal(bookReview: BookReview) {
+    setSelectedBookReview(bookReview);
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
 
   return (
     <div>
@@ -17,12 +30,25 @@ export default function BooksList({ bookReviews }: BooksListProps) {
         const content = translatedReview?.content ?? review.article.originalContent;
 
         return (
-          <div key={review.id} className={booksListStyles.BooksListItem} data-aos={index % 2 ? 'fade-right' : 'fade-left'}>
+          <div
+            key={review.id}
+            className={booksListStyles.BooksListItem}
+            data-aos={index % 2 ? 'fade-right' : 'fade-left'}
+            onClick={() => openModal(review)}
+          >
             <div className={booksListStyles.BooksListItemTitle}>{title}</div>
             <div className={booksListStyles.BooksListItemContent}>{content}</div>
           </div>
         )
       })}
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => closeModal()}
+      >
+        <div>{selectedBookReview?.article.originalTitle}</div>
+        <div>{selectedBookReview?.article.originalContent}</div>
+      </Modal>
     </div>
   )
 }
