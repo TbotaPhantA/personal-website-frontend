9112666bb39491a5370621260b794bec35eeb472
TbotaPhantA <botakiller@mail.ru>
2023-04-28 15:50:20 +0300

    Title was styled

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,8 @@ import logInStyles from '@/styles/pages/Login.module.scss';
 
 export default function LogIn() {
   return (
-    <div className={logInStyles.LogInPage}>Login page</div>
+    <div className={logInStyles.LogInPage}>
+      <h1 className={logInStyles.LogInPageTitle}>Login</h1>
+    </div>
   )
 }
