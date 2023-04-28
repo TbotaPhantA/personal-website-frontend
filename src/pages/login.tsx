9a0a6319d4ee0405c27d9041e9e5ae098ca8f164
TbotaPhantA <botakiller@mail.ru>
2023-04-28 15:56:50 +0300

    login was changed to sing in

--- src/pages/login.tsx
+++
@@ -1,9 +0,0 @@
-import logInStyles from '@/styles/pages/Login.module.scss';
-
-export default function LogIn() {
-  return (
-    <div className={logInStyles.LogInPage}>
-      <h1 className={logInStyles.LogInPageTitle}>Login</h1>
-    </div>
-  )
-}
---
+++ src/pages/signin.tsx
@@ -0,0 +1,9 @@
+import signInStyles from '@/styles/pages/SignIn.module.scss';
+
+export default function SignIn() {
+  return (
+    <div className={signInStyles.SignInPage}>
+      <h1 className={signInStyles.SignInPageTitle}>SIgn In</h1>
+    </div>
+  )
+}
