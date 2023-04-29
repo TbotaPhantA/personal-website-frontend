a0dc2ae1a1cd892481b4660fc189530b6048d00e
TbotaPhantA <botakiller@mail.ru>
2023-04-29 18:12:11 +0000

    state logic was added.

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,21 +1,54 @@
 import signInStyles from '@/styles/pages/SignIn.module.scss';
-import { useState } from 'react';
+import { useState, FormEvent, ChangeEvent } from 'react';
+
+export interface FormValues {
+  username: string;
+  password: string;
+}
 
 export default function SignIn() {
-  const [error, setError] = useState<string>('');
+  const [formValues, setFormValues] = useState<FormValues>({
+    username: '',
+    password: '',
+  })
+  const [errors, setErrors] = useState<string[]>([]);
+  // const auth = useStore(authStore);
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>): void {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  }
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    // const signInResponse = await signIn(formValues);
+    // Save the result using effector
+  }
 
   return (
-    <form className={signInStyles.SignInPage}>
+    <form className={signInStyles.SignInPage} onSubmit={handleSubmit}>
       <h1 className={signInStyles.SignInPageTitle}>SIgn In</h1>
       <label className={signInStyles.SignInLoginLabel}>
-        Login:
-        <input type="text" className={signInStyles.SignInLoginInput}/>
+        Username:
+        <input
+          type="text"
+          name="username"
+          className={signInStyles.SignInUsernameInput}
+          value={formValues.username}
+          onChange={handleInputChange}
+        />
       </label>
       <label className={signInStyles.SignInPasswordLabel}>
         Password:
-        <input type="password" className={signInStyles.SignInPasswordInput}/>
+        <input
+          type="password"
+          name="password"
+          className={signInStyles.SignInPasswordInput}
+          value={formValues.password}
+          onChange={handleInputChange}
+        />
       </label>
-      {error && <p>{error}</p>}
+      {errors.map(e => (<p key={e}>{e}</p>))}
       <button type="submit" className={signInStyles.SignInButton}>Sign In</button>
     </form>
   )
