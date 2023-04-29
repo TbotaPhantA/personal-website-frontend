cbbf3ab70d516f38c73eb2d7d4e6ff050de67cbe
TbotaPhantA <botakiller@mail.ru>
2023-04-29 16:20:42 +0000

    - unstyled inputs - styled button were added.

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,9 +1,22 @@
 import signInStyles from '@/styles/pages/SignIn.module.scss';
+import { useState } from 'react';
 
 export default function SignIn() {
+  const [error, setError] = useState<string>('');
+
   return (
     <div className={signInStyles.SignInPage}>
       <h1 className={signInStyles.SignInPageTitle}>SIgn In</h1>
+      <label>
+        Login:
+        <input type="text"/>
+      </label>
+      <label>
+        Password:
+        <input type="password"/>
+      </label>
+      {error && <p>{error}</p>}
+      <button type="submit" className={signInStyles.SignInButton}>Sign In</button>
     </div>
   )
 }
