531432a1f368a623777b957bc8bfea8d2f42ad06
TbotaPhantA <botakiller@mail.ru>
2023-04-30 14:40:21 +0000

    api instance was added.

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import axios, { CreateAxiosDefaults } from 'axios';
+import config from '@/api/config';
+
+const settings: CreateAxiosDefaults = <axios.CreateAxiosDefaults<any>>{
+  baseURL: config.BASE_URL,
+  timeout: config.BASE_TIMEOUT,
+}
+
+const api = axios.create(settings);
