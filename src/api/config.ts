d492d7a73b14c9209e7345ea4444e7ad2cc786fd
TbotaPhantA <botakiller@mail.ru>
2023-04-30 14:40:14 +0000

    config structure was changed.

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,6 +2,19 @@ const throwRequired = (propName: keyof NodeJS.ProcessEnv): never => {
   throw new Error(`Config property ${propName} is required`);
 };
 
-export default {
-  BASE_URL: process.env.BASE_URL ?? throwRequired('BASE_URL'),
+class Config {
+  BASE_URL: string = process.env.BASE_URL ?? throwRequired('BASE_URL');
+  BASE_TIMEOUT: number = 10000;
+
+  private static instance: Config;
+  private constructor() {}
+
+  public static getInstance(): Config {
+    if (!this.instance) { this.instance = new Config() }
+    return this.instance;
+  }
 }
+
+const config = Config.getInstance();
+
+export default config;
