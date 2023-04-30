0d2113161d8fc5679415bc652b77785e1e7f948c
TbotaPhantA <botakiller@mail.ru>
2023-04-30 23:58:10 +0000

    minor link fixes

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -13,8 +13,8 @@ interface SideBarProps {
 }
 
 export function SideBar({ pageWrapId, outerContainerId }: SideBarProps) {
-  const { locale } = useRouter();
-  const t = chooseTranslation(locale);
+  const router = useRouter();
+  const t = chooseTranslation(router.locale);
 
   return (
     <Menu
@@ -35,10 +35,10 @@ export function SideBar({ pageWrapId, outerContainerId }: SideBarProps) {
       <div className={sideBarStyles.SideBarQuote}>{'"The only way to go fast is to go well."'}</div>
       <div className={sideBarStyles.SideBarQuoteAuthor}>{'Robert C. Martin'}</div>
       <hr className={sideBarStyles.SideBarVerticalLine} />
-      <Link href={'#home'} id={'homeLink'} className={sideBarStyles.BmItem}>{t.sidebar.homeButton}</Link>
-      <Link href={'#about'} id={'aboutLink'} className={sideBarStyles.BmItem}>{t.sidebar.aboutButton}</Link>
-      <Link href={'#books'} id={'booksLink'} className={sideBarStyles.BmItem}>{t.sidebar.booksButton}</Link>
-      <Link href={'#contact'} id={'contactLink'} className={sideBarStyles.BmItem}>{t.sidebar.contactButton}</Link>
+      <Link href={'/#home'} id={'homeLink'} className={sideBarStyles.BmItem}>{t.sidebar.homeButton}</Link>
+      <Link href={'/#about'} id={'aboutLink'} className={sideBarStyles.BmItem}>{t.sidebar.aboutButton}</Link>
+      <Link href={'/#books'} id={'booksLink'} className={sideBarStyles.BmItem}>{t.sidebar.booksButton}</Link>
+      <Link href={'/#contact'} id={'contactLink'} className={sideBarStyles.BmItem}>{t.sidebar.contactButton}</Link>
       <hr className={sideBarStyles.SideBarVerticalLine} />
       <div className={sideBarStyles.SideBarFooter}>
         <SideBarSocialLinks />
