0a5f622cfa6841f7e1bd4a28ec3b89ed9ed35c6b
TbotaPhantA <botakiller@mail.ru>
2023-05-24 21:09:32 +0000

    `accessToken` is being put to Authorization header.

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,4 +8,14 @@ const settings: CreateAxiosDefaults = <CreateAxiosDefaults>{
 
 const api = axios.create(settings);
 
+api.interceptors.request.use(config => {
+  const accessToken = localStorage.getItem('accessToken');
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+})
+
 export default api;
