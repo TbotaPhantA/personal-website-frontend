027fa25c19296862cb511a0e53801881487c0c32
TbotaPhantA <botakiller@mail.ru>
2023-05-24 21:31:14 +0000

    `undefined` was replaced by `null`.

--- src/stores/authStore/authStore.ts
+++ src/stores/authStore/authStore.ts
@@ -2,4 +2,4 @@ import { createDomain, createStore } from 'effector';
 import { User } from '@/shared/types/user';
 
 export const authDomain = createDomain();
-export const $userStore = createStore<User | undefined>(undefined);
+export const $userStore = createStore<User | null>(null);
