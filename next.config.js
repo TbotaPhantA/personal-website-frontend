89f395bf32955b213ad625b166a6aed0394934b5
TbotaPhantA <botakiller@mail.ru>
2025-06-21 20:11:57 +0000

    sass update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    fiber: false,
     includePaths: [path.join(__dirname, 'src/styles')],
   },
   i18n: {
