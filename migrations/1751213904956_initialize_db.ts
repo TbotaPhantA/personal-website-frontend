5dbb082e0d773f53e879861440c86f523976ba67
TbotaPhantA <botakiller@mail.ru>
2025-06-29 18:31:03 +0000

    addition

--- migrations/1751213904956_initialize_db.ts
+++ migrations/1751213904956_initialize_db.ts
@@ -3,21 +3,23 @@ import { Database } from '@/backend/infrastructure/db/schema';
 
 export async function up(db: Kysely<Database>): Promise<void> {
   await db.schema.createTable('book_reviews')
+    .addColumn('book_review_id', 'integer', col => col.primaryKey().autoIncrement())
     .addColumn('language_code', 'text', col => col.notNull())
     .addColumn('book_title', 'text', col => col.notNull())
     .addColumn('review_body', 'text', col => col.notNull())
     .addColumn('image_href', 'text', col => col.notNull())
-    .addPrimaryKeyConstraint('book_review_pk', ['language_code', 'book_title'])
+    .addUniqueConstraint('book_review_unique_constraint', ['language_code', 'book_title'])
     .execute();
 
   await db.schema.createTable('users')
+    .addColumn('user_id', 'integer', col => col.primaryKey().autoIncrement())
     .addColumn('username', 'text', col => col.notNull())
     .addColumn('role', 'text', col => col.notNull())
     .addColumn('salt', 'text', col => col.notNull())
     .addColumn('password_hash', 'text', col => col.notNull())
     .addColumn('access_token_version', 'integer', col => col)
     .addColumn('refresh_token', 'text', col => col)
-    .addPrimaryKeyConstraint('username_pk', ['username'])
+    .addUniqueConstraint('username_pk', ['username'])
     .execute();
 }
 
